d33d6474c3da45323e471a28a9bc8fe4a0747d1b
Patrick Callahan <patricktcallahan18@gmail.com>
2020-07-08 19:55:59 -0700

    Intro R and Qualtrics ppt updates; expand on Qualtrics R coding file

--- Automating LAVA Uploads with R/QualtRics Test Script.R
+++ Automating LAVA Uploads with R/QualtRics Test Script.R
@@ -6,9 +6,33 @@
 qualtRics::qualtrics_api_credentials(api_key = keyringr::decrypt_kc_pw("Qualtrics_API_Key"), 
                                      base_url = "ucsf.co1.qualtrics.com")
 
-qualtRics::all_surveys() %>% View()
+qualtRics::all_surveys() %>% head() %>% View()
+
+qualtRics::fetch_survey("SV_esmOleT034cn2rb", force_request = T) %>% View()
 
 
-qualtRics::fetch_survey("SV_esmOleT034cn2rb", force_request = T)
 
+
+# create list of all tests in my account, and filter for only the active ones
+tests <- qualtRics::all_surveys() %>% 
+  dplyr::filter(isActive == "TRUE") %>% # can't query inactive surveys it seems
+  head()
+
+
+# List approach
+data <- tests %>%
+  pull(id) %>%
+  map(~ qualtRics::fetch_survey(., force_request = T, label = F, convert = F)) %>% # this set up is equivalent to exporting from Qualtrics as numeric. Remove `label` and `convert` arguments to be equivalent to Choice Text output
+  setNames(pull(tests,name))
+data %>%
+  pluck("PPG UDS Telephone Follow Up") %>%
+  View()
+
+
+# Nested tibbles approach
+oink <- tests %>%
+  mutate(data = pull(.,id) %>% map(~ qualtRics::fetch_survey(., force_request = T, label = F, convert = F)))  # this set up is equivalent to exporting from Qualtrics as numeric. Remove `label` and `convert` arguments to be equivalent to Choice Text output
+oink %>%
+  dplyr::filter(name == "PPG UDS Telephone Follow Up") %>%
+  pull(data)
 
