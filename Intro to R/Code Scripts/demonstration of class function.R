0d0dcedd25157a66a23af597ce4b31af5c887fa3
Patrick Callahan <patricktcallahan18@gmail.com>
2020-08-17 18:45:28 -0700

    Overhaul Intro to R folder organization

---
+++ Intro to R/Code Scripts/demonstration of class function.R
@@ -0,0 +1,18 @@
+
+
+
+numbers <- c(5, 4, 3.2, 5)
+class(numbers)
+
+logicals <- c(TRUE, FALSE, F, T)
+class(logicals)
+
+characters <- c("hello","I'm","Pat")
+class(characters)
+
+mixed <- c(TRUE, 5, 3)
+class(mixed)
+
+mixed2 <- c(TRUE, "TRUE", 1)
+class(mixed2)
+
