f00c6b36296c830fbff56440518bd1bd6d6302cd
Patrick Callahan <patricktcallahan18@gmail.com>
2020-08-25 15:22:44 -0700

    Misc. docs to accompany Git/GitHub/RStudio presentation

---
+++ RStudio, Git, and Github/Downloads and Packages Script.R
@@ -0,0 +1,18 @@
+
+# DOWNLOAD GIT
+#   http://git-scm.com/downloads
+
+# GITHUB SIGN UP
+#   https://github.com/
+
+# DOWNLOAD RSTUDIO
+#   https://rstudio.com/products/rstudio/download/
+
+
+install.packages('devtools', dependencies = T)
+install.packages('usethis', dependencies = T)
+install.packages('keyringr', dependencies = T)
+
+library(devtools)
+library(usethis)
+library(devtools)
